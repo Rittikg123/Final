886f99f291d73e3bfcdf40c0e6c246b3692654d4
Rittikg123 <rittikbd1@gmail.com>
2023-12-14 15:48:02 -0500

    Comments have been made for testing branch

--- src/Package1/SelectionSortTest.java
+++ src/Package1/SelectionSortTest.java
@@ -10,6 +10,7 @@ public class SelectionSortTest {
     	SelectionSort selectionSort = new SelectionSort();
     	
     	// Test case 1: Positive integers
+    	//Testing git1
         int[] array1 = {4, 2, 7, 1, 9, 5};
         int[] result1 = {1, 2, 4, 5, 7, 9};
         selectionSort.selectionSort(array1);
@@ -17,12 +18,14 @@ public class SelectionSortTest {
 
 
         // Test case 2: Negative
+        //Testing git2
         int[] array2 = {-4, -5, -10, -2, -3};
         int[] result2 = {-10, -5, -4, -3, -2};
         selectionSort.selectionSort(array2);
         Assert.assertArrayEquals(result2, array2);
 
         // Test case 3: Mixed
+        //Comments have been made for git3
         int[] array3 = {-3, 2, 4, -6, 10};
         int[] result3 = {-6, -3, 2, 4, 10};
         selectionSort.selectionSort(array3);
